9b8800712c0c00e8b2019677126ab338d1cd750f
PedroVMota <pvital-m@student.42lisboa.com>
2024-02-29 19:43:59 +0000

    MariaDb

--- start.sh
+++ start.sh
@@ -1,7 +1,9 @@
 if [[ $1 == "delete" ]]; then
-    docker stop $(docker ps -aq)
-    docker rm $(docker ps -a -q)
-    docker rmi $(docker images -q)
+    docker stop $(docker ps -aq) 2> /dev/null 2> /dev/null
+    docker rm $(docker ps -a -q) 2> /dev/null 2> /dev/null
+    docker rmi $(docker images -q) 3> /dev/null 2> /dev/null
+    docker volume rm $(docker volume ls -q) 2> /dev/null 2> /dev/null
+
     exit 0
 fi
 
